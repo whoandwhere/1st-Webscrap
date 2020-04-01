6f12257ea032bbedbb10c4503a19b5fd79ffd690
urbalke <patryk.urba@gmail.com>
2020-04-01 22:06:49 +0200

    Update Allegro.py

--- Allegro.py
+++ Allegro.py
@@ -13,6 +13,7 @@ import tkinter as tk
 from tkinter import scrolledtext
 import pathlib
 from pathlib import Path
+import tkinter.messagebox
 
 
 
@@ -28,12 +29,12 @@ def clear():
 
 def scrapp():
 
-    for q in range(1, 999):
+    for q in range(1, int(p.get())):
         try:
             page = requests.get(e.get() + "&p=" + str(q))
             soup = BeautifulSoup(page.content, 'html.parser')
             artykuly = soup.find_all('article')
-            for i in range(400):
+            for i in range(61):
                 try:
                     artykul = artykuly[i]
                     tytuly = artykul.h2.extract()
@@ -48,7 +49,7 @@ def scrapp():
                     break
         except:
             break
-
+    return tk.messagebox.showinfo('Koniec', 'Udało się zapisać wszystkie aukcje :) lub nie :D:D')
 
 current_dir = Path.cwd()
 
@@ -58,13 +59,19 @@ root.geometry("500x90")
 root.resizable(0, 0)
 
 label1 = tk.LabelFrame(text="Wklej link poniżej")
+label2 = tk.Label(text="Ile jest stron?")
+
 e = tk.Entry(label1, width="65", borderwidth="5")
+p = tk.Entry(root, width="10", borderwidth="5")
 start_button = tk.Button(root, text="Scrap", padx=25, pady=5, command=scrapp)
 clear_button = tk.Button(root, text="Wyczyść", padx=25, pady=5, command=clear)
 exit_button = tk.Button(root, text="Zakończ", padx=25, pady=5, command=close)
 
+
 label1.grid(row=0, column=0)
 e.grid(row="0", column="0")
+label2.place(x=140,y=52)
+p.place(x=230, y=50)
 start_button.place(x=410, y=10)
 clear_button.place(x=300, y=50)
 exit_button.place(x=400, y=50)
